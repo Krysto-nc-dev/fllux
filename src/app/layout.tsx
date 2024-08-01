22e8b52d9260107165734a8633b4bdbbdb2b22e9
Velten Stoyann <krysto.dev@gmail.com>
2024-08-01 22:38:52 +1100

    price cards home components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from '@/context/theme-provider'
 
-const inter = Inter({ subsets: ['latin'] })
+const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Fllux || Gestion des stock et plus encore !',
-  description: 'Fllux est une application de gestion de stock incluant des outils IA',
+  description:
+    'Fllux est une application de gestion de stock incluant des outils IA',
 }
 
 export default function RootLayout({
@@ -19,9 +21,18 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="fr">
-        <body className={inter.className}>{children}
-<Toaster/>
-
+        <body className={jakarta.className}>
+        
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+     
         </body>
       </html>
     </ClerkProvider>
---
+++ src/constants/landing-page.ts
@@ -0,0 +1,29 @@
+export const pricingCards = [
+  {
+    title: 'Gratuit',
+    description: 'Parfait pour essayer Fluxx',
+    price: '0 XPF',
+    duration: '',
+    highlight: 'Caractéristiques principales',
+    features: ['1 domaine', '10 contacts', '10 e-mails par mois'],
+    priceId: '',
+  },
+  {
+    title: 'Standard',
+    description: 'Le kit ultime pour les petites entreprises',
+    price: '8,000 XPF',
+    duration: 'mois',
+    highlight: 'Caractéristiques principales',
+    features: ['2 domaines', '50 contacts', '50 e-mails par mois'],
+    priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+  },
+  {
+    title: 'Premium',
+    description: 'Le kit ultime pour les grandes entreprises',
+    price: '11,500 XPF',
+    duration: 'mois',
+    highlight: 'Caractéristiques principales',
+    features: ['Domaines illimités', '500 contacts', '500 e-mails par mois'],
+    priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+  },
+]
